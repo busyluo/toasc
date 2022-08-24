f80b3b98f22bf4c5d123cd484832bbe72cff62e4
luoxianfeng <luoxianfeng@idriverplus.com>
2022-08-24 23:59:13 +0800

    fix: 修复对应关系

--- converter.cpp
+++ converter.cpp
@@ -29,23 +29,24 @@ void Converter::Convert(QString input) {
     QStringList path_items = input.split("/");
     QString asc_name = path_items.last().split(".").front();
     QString file_dir = path_items.mid(0, path_items.size() - 1).join("/");
-//    UBusMessageHead_t message_head;
-//    memcpy(&message_head, urc_data.data() + data_ptr, k_msg_head_size);
-//    if (message_head.type == UBT_FLEXRAY) {
-//    } else if (message_head.type == UBT_CAN) {
-//    }
+    //    UBusMessageHead_t message_head;
+    //    memcpy(&message_head, urc_data.data() + data_ptr, k_msg_head_size);
+    //    if (message_head.type == UBT_FLEXRAY) {
+    //    } else if (message_head.type == UBT_CAN) {
+    //    }
 
     Vector::ASC::File asc_file;
     QString asc_file_name = file_dir + "/" + asc_name + ".asc";
     asc_file.open(asc_file_name.toStdString(), File::OpenMode::Write);
-    if(!asc_file.is_open()) {
+    if (!asc_file.is_open()) {
         emit Failed("打开asc文件失败");
         return;
     }
     FileDate date;
     time_t now;
     time(&now);
-    date.date = *localtime(&now);
+    struct tm tm = *localtime(&now);
+    date.date = tm;
     asc_file.write(&date);
 
     FileBaseTimestamps base;
@@ -61,6 +62,7 @@ void Converter::Convert(QString input) {
     asc_file.write(&version);
 
     BeginTriggerblock begin;
+    begin.date = tm;
     asc_file.write(&begin);
 
     while (data_ptr < urc_data.size()) {
@@ -79,8 +81,28 @@ void Converter::Convert(QString input) {
             data_ptr += payload_len;
 
             FlexRayMessage msg;
-            msg.time = message_head.stamp;
+            msg.time = message_head.stamp / 1000000.0;
             msg.data = fr_data;
+            msg.clusterNr = 0;
+            msg.clientId = 0;
+            msg.channelNr = 1;
+            if (message_head.data.frData.ffh.slotStatus & 0x80) {
+                msg.channelMask = 1;
+            } else {
+                msg.channelMask = 2;
+            }
+            msg.slotId = message_head.data.frData.ffh.frameID;
+            msg.cycleNo = message_head.data.frData.ffh.cycleCount;
+            msg.ccType = 5;
+            msg.ccData = message_head.data.frData.ffh.slotStatus;
+            msg.headerCrc = message_head.data.frData.ffh.headerCrc;
+            msg.name = "xxxx";
+            msg.payloadLength = message_head.data.frData.ffh.payloadLength;
+            msg.bufferLength = message_head.data.frData.ffh.payloadLength;
+            msg.frameCrc = 0;
+            msg.spyFlag = 0;
+            msg.frameLengthNs = 0;
+            msg.pduOffset = 0;
             asc_file.write(&msg);
         } else if (message_head.type == UBT_CAN) {
             memcpy(&message_head.data, urc_data.data() + data_ptr, k_can_head_size);
@@ -92,15 +114,32 @@ void Converter::Convert(QString input) {
             data_ptr += payload_len;
             data_ptr += 3;
 
-
-            CanMessage msg;
-            msg.id = message_head.serialNumber;
-            msg.channel = message_head.nodeId;
-            msg.dir = Dir::Rx;
-            msg.dlc = payload_len;
-            msg.time = message_head.stamp;
-            msg.data = can_data;
-            asc_file.write(&msg);
+            if (message_head.data.canData.cs >> 21 & 0x01) {
+                CanExtendedMessage msg;
+                msg.id = message_head.serialNumber;
+                msg.channel = message_head.nodeId;
+                msg.dir = Dir::Rx;
+                msg.dlc = payload_len;
+                msg.time = message_head.stamp / 1000000.0;
+                msg.data = can_data;
+                asc_file.write(&msg);
+            } else if (message_head.data.canData.cs >> 20 & 0x01) {
+                CanRemoteFrame msg;
+                msg.id = message_head.serialNumber;
+                msg.channel = message_head.nodeId;
+                msg.dir = Dir::Rx;
+                msg.time = message_head.stamp / 1000000.0;
+                asc_file.write(&msg);
+            } else {
+                CanMessage msg;
+                msg.id = message_head.serialNumber;
+                msg.channel = message_head.nodeId;
+                msg.dir = Dir::Rx;
+                msg.dlc = payload_len;
+                msg.time = message_head.stamp / 1000000.0;
+                msg.data = can_data;
+                asc_file.write(&msg);
+            }
         } else {
             emit Failed(QString("不支持的总线类型: %1").arg(message_head.type));
             return;
